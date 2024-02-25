caf3e121c1f0196d8c57b2f2796887ba7b590bda
Yuki Ogawa <ogachan6464@gmail.com>
2024-02-24 22:14:22 -0700

    create folder called seattle

---
+++ frontend/app/seattle/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import { Input } from "@nextui-org/input";
+import { useEffect, useState } from "react";
+
+import InfraStatus from "@/components/infraStatus";
+
+export default function Home() {
+  const [infraState, setInfraState] = useState([["", ""]]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/status/Seattle")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      setInfraState(data);
+    })
+    .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className={title()}>Make&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+        <br />
+        <h1 className={title()}>
+          websites regardless of your design experience.
+        </h1>
+        <h2 className={subtitle({ class: "mt-4" })}>
+          Beautiful, fast and modern React UI library.
+        </h2>
+      </div>
+
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          href={"spooky"}
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+        >
+          Documentation
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Snippet hideSymbol hideCopyButton variant="flat">
+          <span>
+            <Input
+              type="search"
+              variant="underlined"
+              label="City"
+              placeholder="Seattle"
+            />
+          </span>
+        </Snippet>
+      </div>
+      <div className="flex">
+        {infraState && <InfraStatus infra={infraState as [string, string][]} />}
+      </div>
+    </section>
+  );
+}
